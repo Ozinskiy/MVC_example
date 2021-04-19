3d00744d171205ed17a48fa51a0124fd568976c1
Ozinskyi <victor.gusev94@yandex.ru>
2021-04-19 15:43:07 +0400

    Initial commit

---
+++ src/main/java/model/MainModel.java
@@ -0,0 +1,55 @@
+package model;
+
+import bean.User;
+import model.service.UserService;
+import model.service.UserServiceImpl;
+
+import java.util.List;
+
+public class MainModel implements Model{
+    private UserService userService = new UserServiceImpl();
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = getAllUsers();
+        modelData.setUsers(users);
+        modelData.setDisplayDeletedUserList(false);
+    }
+
+    public void loadDeletedUsers() {
+        List<User> users = userService.getAllDeletedUsers();
+        modelData.setUsers(users);
+        modelData.setDisplayDeletedUserList(true);
+    }
+
+    public void loadUserById(long userId) {
+        User user = userService.getUsersById(userId);
+        modelData.setActiveUser(user);
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+        userService.deleteUser(id);
+        List<User> users = getAllUsers();
+        modelData.setUsers(users);
+    }
+
+    private List<User> getAllUsers() {
+        List<User> allUsers = userService.getUsersBetweenLevels(1, 100);
+        allUsers = userService.filterOnlyActiveUsers(allUsers);
+        return allUsers;
+    }
+
+
+    public void changeUserData(String name, long id, int level) {
+        userService.createOrUpdateUser(name, id, level);
+        List<User> users = getAllUsers();
+        modelData.setUsers(users);
+    }
+}
---
+++ src/main/java/view/EditUserView.java
@@ -0,0 +1,30 @@
+package view;
+
+import bean.User;
+import controller.Controller;
+import model.ModelData;
+
+public class EditUserView implements View{
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("User to be edited:");
+        User activeUser = modelData.getActiveUser();
+        System.out.println("\t" + activeUser.toString());
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireEventUserDeleted(long id){
+        controller.onUserDelete(id);
+    }
+
+    public void fireEventUserChanged(String name, long id, int level) {
+        controller.onUserChange(name, id, level);
+    }
+}
